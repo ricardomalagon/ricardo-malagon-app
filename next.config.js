da3d4a96cb2cd71d5a1ab8e50ba4cc78b34e405e
Jhovanna Monegro <ricardo@ricardomalagon.com>
2024-03-02 18:28:30 -0400

    Cleaning dirty code

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const withImages = require("next-images");
-// import withImages from "next-images";
-
-// module.exports = withImages({
-//   trailingSlash: true,
-//   output: "export",
-//   reactStrictMode: false,
-//   swcMinify: true,
-//   images: {
-//     disableStaticImages: true,
-//   },
-// });
-
-// import withImages from "next-images";
-
-// const nextConfig = withImages({
-//   output: "export",
-//   trailingSlash: true,
-//   distDir: "dist",
-// });
-
-// export default nextConfig;
 
 const nextConfig = withImages({
   output: "export",
